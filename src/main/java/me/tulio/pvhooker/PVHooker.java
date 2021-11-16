fa036d18e426ea40d909af0009934400f32f78be
TulioTriste <33809410+TulioTriste@users.noreply.github.com>
2021-11-15 23:34:41 -0300

    created config

--- src/main/java/me/tulio/pvhooker/PVHooker.java
+++ src/main/java/me/tulio/pvhooker/PVHooker.java
@@ -1,13 +1,15 @@
 package me.tulio.pvhooker;
 
+import dev.panda.file.FileConfig;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PVHooker extends JavaPlugin {
 
+    private FileConfig config;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        config = new FileConfig(this, "config.yml");
     }
 
     @Override
