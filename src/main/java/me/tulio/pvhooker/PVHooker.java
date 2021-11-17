32724bcd378b1f7be27505a216e50bdcbc41d94b
TulioTriste <33809410+TulioTriste@users.noreply.github.com>
2021-11-17 12:29:50 -0300

    added redis connection

--- src/main/java/me/tulio/pvhooker/PVHooker.java
+++ src/main/java/me/tulio/pvhooker/PVHooker.java
@@ -2,21 +2,30 @@ package me.tulio.pvhooker;
 
 import dev.panda.file.FileConfig;
 import lombok.Getter;
+import me.tulio.pvhooker.redis.Redis;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
 public final class PVHooker extends JavaPlugin {
 
     private FileConfig config;
+    private Redis redis;
 
     @Override
     public void onEnable() {
         config = new FileConfig(this, "config.yml");
+        connectRedis();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void connectRedis() {
+        redis = new Redis();
+        redis.connect();
+        System.out.println("[PVHooker] Redis connected!");
     }
 
     public static PVHooker get() {
